b9c1178356db80bee582d2dc7eb95b912a38530c
Scotty <scotty.fulton01@gmail.com>
2019-11-12 22:06:38 -0600

    API nom not much to commit, just pushing env to show. got a list to display by placing logic in the render b4 the return

--- routes/search.js
+++ routes/search.js
@@ -38,26 +38,26 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // client.search({
-  //   //   term: 'Four Barrel Coffee',
-  //   radius: 20000,
-  //   //   location: 'lat: 29.757241399999998, long:-95.46730649999999',
-  //   latitude: 29.757241399999998,
-  //   longitude: -95.46730649999999,
+//   client.search({
+//     //   term: 'Four Barrel Coffee',
+//     radius: 20000,
+//     //   location: 'lat: 29.757241399999998, long:-95.46730649999999',
+//     latitude: 29.757241399999998,
+//     longitude: -95.46730649999999,
     
-  //   }).then(response => {
-  //       fs.writeFile("./queryResult.json", JSON.stringify(response.jsonBody.businesses), (err) => {
-  //           if (err) {
-  //               console.error(err);
-  //               return;
-  //           };
-  //           console.log("File has been created");
-  //       });
-  //       // res.send(response.jsonBody.businesses);
-  //     console.log(response.jsonBody.businesses[0].name);
-  //   }).catch(e => {
-  //     console.log(e);
-  //   });
+//     }).then(response => {
+//         fs.writeFile("./queryResult.json", JSON.stringify(response.jsonBody.businesses), (err) => {
+//             if (err) {
+//                 console.error(err);
+//                 return;
+//             };
+//             console.log("File has been created");
+//         });
+//         res.send(response.jsonBody.businesses);
+//       console.log(response.jsonBody.businesses[0].name);
+//     }).catch(e => {
+//       console.log(e);
+    // });
   res.send(json);
 });
 

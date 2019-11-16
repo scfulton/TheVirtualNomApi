777e7df3aa25dba71fcf9070bb0279e77ba74254
Scotty Fulton <sfulton@talentpath.com>
2019-11-15 18:21:14 -0600

    MVPgit add .git add . v1

--- routes/search.js
+++ routes/search.js
@@ -38,27 +38,35 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-//   client.search({
-//     //   term: 'Four Barrel Coffee',
-//     radius: 20000,
-//     //   location: 'lat: 29.757241399999998, long:-95.46730649999999',
-//     latitude: 29.757241399999998,
-//     longitude: -95.46730649999999,
+  let addy = req.query.location;
+  console.log('addy:', addy)
+  // console.log("loc: ", address, "| radius:", rad)
+  console.log("loc: ", req.query.location, "| radius:", req.query.radius)
+  client.search({
+    //   term: 'Four Barrel Coffee',
+    // radius: 20000,
+    //   location: 'lat: 29.757241399999998, long:-95.46730649999999',
+    // latitude: 29.757241399999998,
+    // longitude: -95.46730649999999,
+    location : req.query.location,
+    radius : req.query.radius,
+    // console.log("loc: ", location, "| radius:", radius)
     
-//     }).then(response => {
-//         fs.writeFile("./queryResult.json", JSON.stringify(response.jsonBody.businesses), (err) => {
-//             if (err) {
-//                 console.error(err);
-//                 return;
-//             };
-//             console.log("File has been created");
-//         });
-//         res.send(response.jsonBody.businesses);
-//       console.log(response.jsonBody.businesses[0].name);
-//     }).catch(e => {
-//       console.log(e);
-    // });
-  res.send(json);
+    
+    }).then(response => {
+        fs.writeFile("./queryResult.json", JSON.stringify(response.jsonBody.businesses), (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            };
+            console.log("File has been created");
+        });
+        res.send(response.jsonBody.businesses);
+      console.log(response.jsonBody.businesses[0].name);
+    }).catch(e => {
+      console.log(e);
+    });
+  // res.send(json);
 });
 
 module.exports = router;

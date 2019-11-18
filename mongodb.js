36f26703d85a5961b19e4abca9de9cf46783097c
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-11-18 12:31:34 -0600

    updated database file and .env

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,13 @@
 const MongoClient = require("mongodb").MongoClient;
 var json = require("./queryResult.json");
+require("dotenv").config();
+const mongodb_key = process.env.MONGODB_KEY; //contains the username and password for mongodb atlas connection
 
-const uri =
-  "mongodb+srv://Trevis42:Uy8q7l%21Uavu%5EgOOB@virtualnom-cluster-sqmcw.mongodb.net/test?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${mongodb_key}@virtualnom-cluster-sqmcw.mongodb.net/test?retryWrites=true&w=majority`; //use for connecting to cloud server for mongodb
+//const uri = ''; use this for local dev...might be best for quick testing so we dont connect too often to cloud server
+
+const userName = "Test1"; //we will get this from else where (used for testing)
+const password = "securePswd"; //we will get this from else where (used for testing)
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -14,8 +19,14 @@ client.connect(err => {
   // perform actions on the collection object
   console.log("connected...");
   console.log(collection.collectionName.toString());
-  console.log(json);
 
-  collection.insertOne(json);
+  //collection.insertOne(json);
+  collection.insertOne({
+    username: userName,
+    password: password,
+    qResult: json
+  });
+
+  console.error(err);
   client.close();
 });
